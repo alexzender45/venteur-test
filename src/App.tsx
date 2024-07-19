003228e33ddce562179c794d951b9607b5b191d7
alexzender45 <alexzendersamuel33@gmail.com>
2024-07-19 22:39:49 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const App: React.FC = () => {
                 console.log(r)
             );
         }
-    }, []);
+    }, [setRequestHistory]);
 
     const saveStateToLocalStorage = (state: any) => {
         localStorage.setItem('wordleState', JSON.stringify(state));
